07bb4780e9f807c00b57f1a134687bc48911adc9
ima-69 <imansha.idr@gmail.com>
2026-02-14 14:28:06 +0530

    feat(user-service): add skill management functionality with add and delete operations

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuth } from '../middleware/auth.js';
-import { getUserProfile, myProfile, updateProfilePicture, updateResume, updateUserProfile } from '../controllers/user.js';
+import { addSkillToUser, deleteSkillFromUser, getUserProfile, myProfile, updateProfilePicture, updateResume, updateUserProfile } from '../controllers/user.js';
 import uploadFile from '../middleware/multer.js';
 
 const router = express.Router();
@@ -10,6 +10,10 @@ router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
 router.put("/update/profile-pic", isAuth, uploadFile, updateProfilePicture);
 router.put("/update/resume", isAuth, uploadFile, updateResume);
+router.post("/skill/add", isAuth, addSkillToUser);
+router.delete("/skill/delete", isAuth, deleteSkillFromUser);
+
+
 
 
 export default router;
