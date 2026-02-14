3d42ff32efd7402bca421b0ac64b465face97ad6
ima-69 <imansha.idr@gmail.com>
2026-02-14 13:00:39 +0530

    feat(user-service): add profile picture update functionality with file upload handling

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { isAuth } from '../middleware/auth.js';
-import { getUserProfile, myProfile, updateUserProfile } from '../controllers/user.js';
+import { getUserProfile, myProfile, updateProfilePicture, updateUserProfile } from '../controllers/user.js';
+import uploadFile from '../middleware/multer.js';
 
 const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
+router.put("/update/profile-pic", isAuth, uploadFile, updateProfilePicture);
+
 
 export default router;
