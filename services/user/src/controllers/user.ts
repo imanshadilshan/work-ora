53916bfa125b86fb1990078211e5dedd58e7aa09
ima-69 <imansha.idr@gmail.com>
2026-02-14 11:22:25 +0530

    feat(user-service): implement update user profile functionality with validation and response handling

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -34,3 +34,29 @@ export const getUserProfile = TryCatch(async (req, res, next) => {
 
   res.json(user);
 });
+
+export const updateUserProfile = TryCatch(async (req: AuthenticatedRequest, res) => {
+    const user = req.user;
+
+    if (!user) {
+        throw new ErrorHandler(401, "Authenticated user required");
+    }
+
+    const { name, phone_number, bio } = req.body;
+
+    const newName = name || user.name;
+    const newPhoneNumber = phone_number || user.phone_number;
+    const newBio = bio || user.bio;
+
+    const updatedUser = await sql`
+        UPDATE users
+        SET name = ${newName}, phone_number = ${newPhoneNumber}, bio = ${newBio}
+        WHERE user_id = ${user.user_id}
+        RETURNING user_id, name, email, phone_number, bio
+    `;
+
+    res.json({
+        message: "Profile updated successfully",
+        updatedUser,
+    })
+})
